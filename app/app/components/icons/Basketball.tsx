31df4842afb6c273fc5e45d60ff6002a26a4b0c0
777Rob <rcekanauskasnl@gmail.com>
2025-12-26 22:27:55 +0200

    Add Icons

---
+++ app/app/components/icons/Basketball.tsx
@@ -0,0 +1,33 @@
+const Basketball = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      strokeWidth="2"
+      fill="none"
+    />
+    <path d="M12 2C12 2 12 22 12 22" stroke="currentColor" strokeWidth="2" />
+    <path d="M2 12C2 12 22 12 22 12" stroke="currentColor" strokeWidth="2" />
+    <path
+      d="M19.5 4.5C19.5 4.5 16 8 16 12C16 16 19.5 19.5 19.5 19.5"
+      stroke="currentColor"
+      strokeWidth="2"
+      fill="none"
+    />
+    <path
+      d="M4.5 4.5C4.5 4.5 8 8 8 12C8 16 4.5 19.5 4.5 19.5"
+      stroke="currentColor"
+      strokeWidth="2"
+      fill="none"
+    />
+  </svg>
+);
+export default Basketball;
---
+++ app/app/components/icons/TennisBall.tsx
@@ -0,0 +1,30 @@
+const TennisBall = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      strokeWidth="2"
+      fill="none"
+    />
+    <path
+      d="M6 3.5C6 3.5 10 8 10 12C10 16 6 20.5 6 20.5"
+      stroke="currentColor"
+      strokeWidth="2"
+      fill="none"
+    />
+    <path
+      d="M18 3.5C18 3.5 14 8 14 12C14 16 18 20.5 18 20.5"
+      stroke="currentColor"
+      strokeWidth="2"
+      fill="none"
+    />
+  </svg>
+);
